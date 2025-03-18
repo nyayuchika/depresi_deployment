53c8075ee784ae7fce27c602eb07590d0448b573
Nyayu Chika Marselina <nyayuchikamsln@gmail.com>
2025-03-18 13:27:47 +0700

    update ml and dl

--- deploy.py
+++ deploy.py
@@ -28,8 +28,8 @@ csp = {
 }
 Talisman(app, content_security_policy=csp)
 
-#load dataset
-files = os.path.join('./*.csv')
+# load dataset
+files = os.path.join('dataset/*.csv')
 files = glob.glob(files)
 dataset = pd.concat(map(lambda f: pd.read_csv(f, encoding='latin-1'), files), ignore_index=True)
 dataset.columns = ['text', 'label']
@@ -53,11 +53,12 @@ def home():
 def about():
     return render_template('./about.html', **locals())
 
-@app.route('/ml', methods=['POST', 'GET'])
+@app.route('/ml', methods=['POST','GET'])
 def ml():
     #ngambil input
-    text_input = request.form.get('text_input','').strip()
-    
+    # text_input = request.form.get('text_input','').strip()
+    text_input = "cemas dan khawatir sama masa depan"
+
     #lowercase
     lowercase = text_input.lower()
    
@@ -74,12 +75,12 @@ def ml():
     normalize_alay = normalize_alay(punctuation)
    
     #remove stopwords
-    nltk.download('punk')
+    nltk.download('punkt')
     nltk.download('stopwords')
     text_tokens = word_tokenize(normalize_alay)
     tokens_without_sw = [word for word in text_tokens if not word in stopwords.words()]
-    filtered_sentence = [" ".join(tokens_without_sw)]
-    # filtered_sentence = [filtered_sentence]
+    filtered_sentence = (" ").join(tokens_without_sw)
+    filtered_sentence = [filtered_sentence]
 
     #convert input
     text_transformed = vectorizer.transform(filtered_sentence)
@@ -99,12 +100,34 @@ def ml():
 @app.route('/dl', methods=['POST', 'GET'])
 def dl():
     #ngambil input
-    text_input = request.form.get('text_input2','').strip()
-    text_input = [text_input]
-    text_input = np.array(text_input)
+    # text_input = request.form.get('text_input2','').strip()
+    text_input = "cemas dan khawatir sama masa depan"
+
+    #lowercase
+    lowercase = text_input.lower()
+   
+    #remove punct
+    import re
+    punctuation = re.sub("[^\w\s\d]","",lowercase)
+   
+    #convert slang
+    alay_dict = pd.read_csv('new_kamusalay.csv', encoding='latin-1', header=None)
+    alay_dict = alay_dict.rename(columns={0:'original', 1:'replacement'})
+    alay_dict_map = dict(zip(alay_dict['original'], alay_dict['replacement']))
+    def normalize_alay(text):
+        return " ".join([alay_dict_map[word] if word in alay_dict_map else word for word in text.split()])
+    normalize_alay = normalize_alay(punctuation)
+   
+    #remove stopwords
+    nltk.download('punkt')
+    nltk.download('stopwords')
+    text_tokens = word_tokenize(normalize_alay)
+    tokens_without_sw = [word for word in text_tokens if not word in stopwords.words()]
+    filtered_sentence = (" ").join(tokens_without_sw)
+    filtered_sentence = [filtered_sentence]
 
     #tokenizer
-    sequences = tokenizer.texts_to_sequences([text_input])
+    sequences = tokenizer.texts_to_sequences(filtered_sentence)
 
     #pad sequence
     padded = pad_sequences(sequences, maxlen=40, padding='post', truncating='post')
@@ -121,4 +144,4 @@ def dl():
     return render_template('./deeplearning.html', **locals())
 
 if __name__ == '__main__':
-    app.run(debug=True, threaded=True)
+    app.run(debug=True)
